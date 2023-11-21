1d39a2cb21114553ddfb7b981b1f8f317724e6ee
SoulSubmerge <Mywayistheseaofstars@outlook.com>
2023-11-21 16:59:45 +0800

    Change the code for simply printing characters to the screen.

--- src/boot.asm
+++ src/boot.asm
@@ -2,7 +2,7 @@
 
 ; 编译指令： nasm -f bin boot.asm -o boot.bin
 ; 设置屏幕模式为文本模式，清除屏幕
-mov ax, 3
+mov ax, 0x0003 ; AH=0x00 设置模式 AL=0x03 文字 80*25 颜色2
 int 0x10
 
 ; 初始化段寄存器
@@ -12,10 +12,20 @@ mov es, ax
 mov ss, ax
 mov sp, 0x7c00
 
-; 0xb8000 文本显示器的内存区域
-mov ax, 0xb800
-mov ds, ax
-mov byte [0], 'H'
+; 显示文本
+mov ah, 0x0E
+mov al, 'S'
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, 'u'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'O'
+int 0x10
+mov al, 'S'
+int 0x10
 
 
 ; 阻塞
